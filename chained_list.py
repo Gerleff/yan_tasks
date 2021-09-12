3734ad66693bfddb1cd9b5c7cf12342b2df87f27
German Goltsov <9150654-GerGoltz@users.noreply.gitlab.com>
2021-09-12 19:45:33 +0300

    first commit: chained list task

---
+++ chained_list.py
@@ -0,0 +1,5 @@
+"""Yandex chained list interview task.
+Condition:
+You need to write down a function which receives the first element of chained list.
+Function should reverse it and return new first element of chained list. """
+
