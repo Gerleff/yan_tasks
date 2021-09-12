91acbd2f02541f6ebe4a1ce319eb9d61e754cb81
German Goltsov <9150654-GerGoltz@users.noreply.gitlab.com>
2021-09-12 20:43:26 +0300

    first commit: chained list task compete + TODOs

--- chained_list.py
+++ chained_list.py
@@ -4,24 +4,24 @@ You need to write down a function which receives the first element of chained li
 Function should reverse it and return new first element of chained list. """
 
 
-class ChainedElem:
+class ChainedElem:  # TODO Create child class that has __str__ method which is recursive. That class must be created
+    # from parent.
     """Single element of chained list. If it's the last element next_elem is None."""
     def __init__(self, value, next_elem=None):
         self.value = value
         self.next_elem = next_elem
 
-    def __str__(self):
-        return f'{self.value}, next - {self.next_elem.value}'
+    def __repr__(self):
+        try:
+            return f'{self.value}, next - {self.next_elem.value}'
+        except AttributeError:
+            return f'{self.value}'
 
     def __call__(self):
         print(self)
 
-
-d = ChainedElem('D')
-c = ChainedElem('C', next_elem=d)
-b = ChainedElem('B', next_elem=c)
-a = ChainedElem('A', next_elem=b)
-
+    # TODO make __add__ method that adds new element in chained list in front of first and returns new first element
+    # TODO make __len__ method that returns count of elements of chained list
 
 
 def create_chained_list(*args):
@@ -31,5 +31,21 @@ def create_chained_list(*args):
         chained = ChainedElem(elem, _next)
     return chained
 
-example = create_chained_list('A', 'B', 'C')
-example()
+
+example = create_chained_list('A', 'B', 'C', 'D')
+
+
+def reverse_chained_list(first: ChainedElem) -> ChainedElem:
+    left, right = None, first
+    while True:
+        print(f'{left = }, {right = }')
+        elem = right.next_elem
+        print(f'{elem = }')
+        right.next_elem = left
+        if not elem:
+            return right
+        left = right
+        right = elem
+
+
+reverse_chained_list(example)()
