08f967aa6ae6ffd8524dba0e7c10f3619a48d2b9
German Goltsov <9150654-GerGoltz@users.noreply.gitlab.com>
2021-09-12 19:54:18 +0300

    first commit: chained list task

--- chained_list.py
+++ chained_list.py
@@ -3,3 +3,15 @@ Condition:
 You need to write down a function which receives the first element of chained list.
 Function should reverse it and return new first element of chained list. """
 
+
+class ChainedElem:
+    """Single element of chained list. If it's the last element next_elem is None."""
+    def __init__(self, value, next_elem=None):
+        self.value = value
+        self.next_elem = next_elem
+
+
+d = ChainedElem('D')
+c = ChainedElem('C', next_elem=d)
+b = ChainedElem('B', next_elem=c)
+a = ChainedElem('A', next_elem=b)
