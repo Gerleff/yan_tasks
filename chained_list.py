531e778d45e463cde087b7f7bdfd0e99172462ce
sofi07 <sofiyatimush@gmail.com>
2021-09-19 19:04:39 +0300

    q

--- chained_list.py
+++ chained_list.py
@@ -4,7 +4,7 @@ You need to write down a function which receives the first element of chained li
 Function should reverse it and return new first element of chained list. """
 
 
-class ChainedElem:  # TODO Create child class that has __str__ method which is recursive. That class must be created
+class ChainedElem(list):  # TODO Create child class that has __str__ method which is recursive. That class must be created
     # from parent.
     """Single element of chained list. If it's the last element next_elem is None."""
     def __init__(self, value, next_elem=None):
@@ -19,6 +19,22 @@ class ChainedElem:  # TODO Create child class that has __str__ method which is r
 
     def __call__(self):
         print(self)
+
+
+    def __add__(self, x):
+        self.insert(0, x)
+        print(self[0])
+
+
+
+class ChildChainedElem(ChainedElem):
+
+    def __init__(self, value, next_elem=None):
+        self.value = value
+        self.next_elem = next_elem
+
+    def __str__(self):
+        print(f'value = {self.value}, next elem = {self.next_elem}.')
 
     # TODO make __add__ method that adds new element in chained list in front of first and returns new first element
     # TODO make __len__ method that returns count of elements of chained list
@@ -49,3 +65,5 @@ def reverse_chained_list(first: ChainedElem) -> ChainedElem:
 
 
 reverse_chained_list(example)()
+
+example.__add__('E')
